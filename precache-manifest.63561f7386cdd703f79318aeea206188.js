ab2bccdcf04a9a496e989041553c5be2953c3ff2
Odezulu Chukwuemeka <odezuluaugustine@gmail.com>
2022-10-01 13:58:02 +0100

    my first commit

---
+++ precache-manifest.63561f7386cdd703f79318aeea206188.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b26c942e91f3f9ab2e1",
+    "url": "/static/css/main.43040b37.chunk.css"
+  },
+  {
+    "revision": "1b26c942e91f3f9ab2e1",
+    "url": "/static/js/main.1b26c942.chunk.js"
+  },
+  {
+    "revision": "52b2bf0a1562300feadb",
+    "url": "/static/css/1.a9d13d4f.chunk.css"
+  },
+  {
+    "revision": "52b2bf0a1562300feadb",
+    "url": "/static/js/1.52b2bf0a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "8cfeb8fd2527dc4cafc8f4bc61d21cdf",
+    "url": "/index.html"
+  }
+];
